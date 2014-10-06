094982726c48f6890bfa45e63923394ea5ae6a63
Roman Degtyarev <rdegtiarev@gmail.com>
2014-10-06 19:03:02 +0400

    Creepy code fragment fixed

--- src/main/scala/HeaderParser.scala
+++ src/main/scala/HeaderParser.scala
@@ -4,19 +4,24 @@ import java.util.regex.Pattern
 /**
  * Created by roman on 25/09/14.
  */
-class HeaderParser(title: String) {
-  val m = Pattern.compile("^(.*)_(E\\d*)_(C\\d*)(?:_(\\d*))*$").matcher(title)
-  if (!m.matches) {
-    throw new ParseException("Wrong header - no match to: " + title, -1)
+class HeaderParser(val src: String) {
+
+  val (variableName, exam, frame) = getHeaderFields(src)
+
+  def getHeaderFields(src: String): (String, String, String) = {
+    val m = Pattern.compile("^(.*)_(E\\d*)_(C\\d*)(?:_(\\d*))*$").matcher(src)
+    if (!m.matches) {
+      throw new ParseException("Wrong header - no match to: " + src, -1)
+    }
+    if (m.groupCount != 4 && m.groupCount != 5) {
+      throw new ParseException("Wrong header group count: " + m.groupCount.toString, -1)
+    }
+    val variableName = if(m.group(4) != null){ m.group(1) + "_" + m.group(4) } else { m.group(1) }
+    val exam = m.group(2)
+    val frame = m.group(3)
+    (variableName, exam, frame)
   }
-  if (m.groupCount != 4 && m.groupCount != 5) {
-    throw new ParseException("Wrong header group count: " + m.groupCount.toString, -1)
-  }
-  val src = title
-  val grps = m.groupCount()
-  val variableName = if(m.group(4) != null){ m.group(1) + "_" + m.group(4) } else { m.group(1) }
-  val exam = m.group(2)
-  val frame = m.group(3)
+
 
   def isSameTitle(b:HeaderParser): Boolean = {
     if(variableName == b.variableName && exam == b.exam)
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,16 @@
+import scala.io.Source
+
 /**
  * Created by roman on 25/09/14.
  */
 
 object main {
   def main(args: Array[String]): Unit = {
-    val reader = new Reader
-    val lines, cols = reader.load(args(0), args(1))
+    val table = Source.fromFile(args(0)).getLines().filter(!_.isEmpty).map(_.split("\t", -1)).toArray
+    val meta = table.filter(_.length<=3)
+    val data = table.filter(_.length>3)
+    val dataset = new Dataset(data, meta)
+    dataset.getMergedTable
+    dataset.makeFile(args(1))
   }
 }
