8673aa6e30677a3d8118bedb2717f1dc11ffc2c6
Roman Degtyarev <rdegtiarev@gmail.com>
2014-10-16 23:02:55 +0400

    Test reworked again

--- src/test/scala/my/com/DatasetTest.scala
+++ src/test/scala/my/com/DatasetTest.scala
@@ -14,12 +14,24 @@ class DatasetTest extends FlatSpec with Matchers {
   val data = source.filter(_.length > 3)
 
   val dataset = new Dataset(meta, data)
+  it should "load data properly" in {
+    val rows = dataset.data("E2").data("C1").getRows
+    rows(1) should be ("c1")
+    rows(2) should be ("c2")
+    rows(3) should be ("c3")
+    rows(4) should be ("c4")
+    rows(5) should be ("c5")
+  }
 
   it should "merge frames from one event by value name" in {
     val merged = dataset.getMerged
-    val table = merged.getRows
+    val rows = merged.getRows
 
-    table(0) should be ("ID\tSB\tA1_E1_C2\tA2_E1_C2\tA1_E2_C1")
-    table(1) should be ("id1\tsb1\ta1\tb1\tc1")
+    rows(0) should be ("ID\tSB\tA1_E1_C2\tA2_E1_C2\tA1_E2_C1")
+    rows(1) should be ("id1\tsb1\ta1\tb1\tc1")
+    rows(2) should be ("id2\tsb2\ta2\tb2\tc2")
+    rows(3) should be ("id3\tsb3\ta3\tb3\tc3")
+    rows(4) should be ("id4\tsb4\ta4\tb4\tc4")
+    rows(5) should be ("id5\tsb5\ta5\tb5\tc5")
   }
 }
