aee942be3c63f6e08e6964753b44bf0cbfca1005
Roman Degtyarev <rdegtiarev@gmail.com>
2014-10-16 22:52:37 +0400

    Test reworked

--- src/test/scala/my/com/FrameTest.scala
+++ src/test/scala/my/com/FrameTest.scala
@@ -2,31 +2,43 @@ package my.com
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.io.Source
+
 /**
  * Created by roman on 28/09/14.
  */
 class FrameTest extends FlatSpec with Matchers {
-  val data1 = Array.ofDim[String](5,5)
-  val data2 = Array.ofDim[String](5,4)
+  val path = getClass.getResource("/testData.tsv")
+  val source = Source.fromURL(path).getLines().filter(!_.isEmpty).map(_.split("\t", -1)).toArray
+  val meta = source.filter(_.length <= 3)
+  val data = source.filter(_.length > 3)
 
-  data1(0) = Array("A1_E1_C1", "A2_E1_C1",  "A3_E1_C1", "A4_E1_C1", "A6_E1_C1")
-  data1(1) = Array(      "A1",         "",        "C1",       "D1",       "D1")
-  data1(2) = Array(        "",       "B2",          "",       "D2",       "D2")
-  data1(3) = Array(      "A3",      "B31",       "C32",       "D3",       "D3")
-  data1(4) = Array(      "A4",         "",        "C4",       "D4",       "D4")
+  val dataset = new Dataset(meta, data)
 
-  data2(0) = Array("A2_E1_C2",  "A1_E1_C2", "A3_E1_C2",  "A5_E1_C2")
-  data2(1) = Array(        "B1",        "",         "",        "E1")
-  data2(2) = Array(      "",          "A2",       "C2",          "")
-  data2(3) = Array(        "",          "",         "",          "")
-  data2(4) = Array(        "B4",        "",         "",        "E4")
+  it should "split data by frames" in {
+    val frame1 = dataset.data("E1").data("C1").getRows
+    val frame2 = dataset.data("E1").data("C2").getRows
 
-  val frame1 = new Frame("C1", data1)
-  val frame2 = new Frame("C2", data2)
+    frame1(1) should be ("a1\tb1")
+    frame1(2) should be ("a2\tb2")
+    frame1(3) should be ("\t")
+    frame1(4) should be ("a4\tb4")
+    frame1(5) should be ("\t")
 
-  "my.com.Frame" should "merge data from two frames into one" in {
-    val merged = frame1 + frame2
-    merged.data(0)(0) should be ("A1_E1_C2")
-    merged.data(0)(5) should be ("A5_E1_C2")
+    frame2(1) should be ("\t")
+    frame2(2) should be ("\t")
+    frame2(3) should be ("a3\tb3")
+    frame2(4) should be ("\t")
+    frame2(5) should be ("a5\tb5")
+  }
+
+  it should "merge data from two frames into one" in {
+    val merged = dataset.data("E1").data("C1") + dataset.data("E1").data("C2")
+    val rows = merged.getRows
+    rows(1) should be ("a1\tb1")
+    rows(2) should be ("a2\tb2")
+    rows(3) should be ("a3\tb3")
+    rows(4) should be ("a4\tb4")
+    rows(5) should be ("a5\tb5")
   }
 }
